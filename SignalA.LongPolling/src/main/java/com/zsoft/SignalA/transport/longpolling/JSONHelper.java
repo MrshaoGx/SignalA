c99ee207ca6c9ac80911246cd7d7754fb5240265
mrsgx <a892607124@qq.com>
2018-02-15 23:33:27 +0800

    fix a little bug when Server exit lead to Client crash. Gradle version update to 4.5

--- SignalA.LongPolling/src/main/java/com/zsoft/SignalA/transport/longpolling/JSONHelper.java
+++ SignalA.LongPolling/src/main/java/com/zsoft/SignalA/transport/longpolling/JSONHelper.java
@@ -7,11 +7,14 @@ public class JSONHelper {
 
 	public static JSONObject ToJSONObject(String text)
 	{
+		if(text==null||text.equals(""))
+			return null;
 		JSONObject json;
 		try {
 			json = new JSONObject(text);
 		} catch (JSONException e) {
 			json = null;
+			e.printStackTrace();
 		}
 		return json;
 	}
